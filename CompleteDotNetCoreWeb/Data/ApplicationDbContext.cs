09917cb2c17cf379f4ead47fe7aca56d1c0a1611
Scott Isitt <isitts@hotmail.com>
2022-09-19 19:24:42 -0700

    Add DbContext and connection string to Program.cs builder.

--- CompleteDotNetCoreWeb/Data/ApplicationDbContext.cs
+++ CompleteDotNetCoreWeb/Data/ApplicationDbContext.cs
@@ -1,13 +1,16 @@
 ﻿using System;
 using Microsoft.EntityFrameworkCore;
+using CompleteDotNetCoreWeb.Models;
 
 namespace CompleteDotNetCoreWeb.Data
 {
     public class ApplicationDbContext : DbContext
     {
-        public ApplicationDbContext()
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
         }
+        // Creates the Category table
+        public DbSet<Category> Categories { get; set; }
     }
 }
 
--- CompleteDotNetCoreWeb/Program.cs
+++ CompleteDotNetCoreWeb/Program.cs
@@ -1,7 +1,11 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using CompleteDotNetCoreWeb.Data;
+using Microsoft.EntityFrameworkCore;
 
-// Add services to the container. <-- Dependency injectionn
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container. <-- Dependency injection
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
