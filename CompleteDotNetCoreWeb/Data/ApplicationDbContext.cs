4d57aa66f3e74ff9f0ff8e8c5979606fe0732d56
Scott Isitt <isitts@hotmail.com>
2022-09-19 18:18:57 -0700

    Add Entity Framework to your project.

---
+++ CompleteDotNetCoreWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace CompleteDotNetCoreWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext()
+        {
+        }
+    }
+}
+
