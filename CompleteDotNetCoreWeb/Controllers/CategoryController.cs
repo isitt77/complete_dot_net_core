53bd90a28bd3eb84fbb8fa48154067fd7f7fd1da
Scott Isitt <isitts@hotmail.com>
2022-09-22 00:24:02 -0700

    Retrieve data in CategoryController via DbContext.

--- CompleteDotNetCoreWeb/Controllers/CategoryController.cs
+++ CompleteDotNetCoreWeb/Controllers/CategoryController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CompleteDotNetCoreWeb.Data;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -10,9 +11,17 @@ namespace CompleteDotNetCoreWeb.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
         // GET: /<controller>/
         public IActionResult Index()
         {
+            List<Models.Category> objectCategoryList = _db.Categories.ToList();
             return View();
         }
     }
