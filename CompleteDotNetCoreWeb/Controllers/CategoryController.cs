bc85e5b513a17411a3c2e675c5bbedfbe9bbf940
Scott Isitt <isitts@hotmail.com>
2022-09-22 00:31:49 -0700

    Change List<Models.Category> to IEnumerable<Category>

--- CompleteDotNetCoreWeb/Controllers/CategoryController.cs
+++ CompleteDotNetCoreWeb/Controllers/CategoryController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using CompleteDotNetCoreWeb.Data;
+using CompleteDotNetCoreWeb.Models;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -21,8 +22,8 @@ namespace CompleteDotNetCoreWeb.Controllers
         // GET: /<controller>/
         public IActionResult Index()
         {
-            List<Models.Category> objectCategoryList = _db.Categories.ToList();
-            return View();
+            IEnumerable<Category> objectCategoryList = _db.Categories.ToList();
+            return View(objectCategoryList);
         }
     }
 }
