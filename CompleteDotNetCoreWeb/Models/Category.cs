ad47a69d515488d3f36e05c26c6bbb50bd919fec
Scott Isitt <isitts@hotmail.com>
2022-09-25 01:19:53 -0700

    Add DisplayName attribute and Range attribute for DisplayOrder in Model.

--- CompleteDotNetCoreWeb/Models/Category.cs
+++ CompleteDotNetCoreWeb/Models/Category.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
 namespace CompleteDotNetCoreWeb.Models
@@ -9,6 +10,8 @@ namespace CompleteDotNetCoreWeb.Models
         public int Id { get; set; }
         [Required]
         public string Name { get; set; } = "";
+        [DisplayName("Display Order")]
+        [Range(1, 100)]
         public int DisplayOrder { get; set; }
         public DateTime CreatedDateTime { get; set; } = DateTime.UtcNow;
     }
