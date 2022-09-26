1bf9681ba435cd59a1e6c19b7decd62fcbbccbed
Scott Isitt <isitts@hotmail.com>
2022-09-25 17:59:39 -0700

    Create Edit GET route and view page.

--- CompleteDotNetCoreWeb/Controllers/CategoryController.cs
+++ CompleteDotNetCoreWeb/Controllers/CategoryController.cs
@@ -52,6 +52,42 @@ namespace CompleteDotNetCoreWeb.Controllers
             }
             return View(obj);
         }
+
+        // GET: 
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var categoryFromDb = _db.Categories.Find(id);
+
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+
+        // Post:
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("CustomError", "DisplayOrder cannot match Name.");
+            }
+
+            Console.WriteLine("isValidModelState: " + ModelState.IsValid);
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
     }
 }
 
