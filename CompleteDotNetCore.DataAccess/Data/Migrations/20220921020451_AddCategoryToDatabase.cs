48096249508ca186c1c0d4207e077568850666cc
Scott Isitt <isitts@hotmail.com>
2022-11-04 00:10:33 -0700

    Change migrations to use DataAccess.

--- CompleteDotNetCore.DataAccess/Data/Migrations/20220921020451_AddCategoryToDatabase.cs
+++ CompleteDotNetCore.DataAccess/Data/Migrations/20220921020451_AddCategoryToDatabase.cs
@@ -4,7 +4,7 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 #nullable disable
 
-namespace CompleteDotNetCoreWeb.Data.Migrations
+namespace CompleteDotNetCore.DataAccess.Migrations
 {
     public partial class AddCategoryToDatabase : Migration
     {
