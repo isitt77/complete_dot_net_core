1af4ba962d8af7e6427cf98e536f15699750df93
Scott Isitt <isitts@hotmail.com>
2022-11-06 22:39:47 -0800

    Create Repository class in Repository directory in DataAccess project. Implement IRepository and add constructor logic to access the database.

---
+++ CompleteDotNetCore.DataAccess/Repository/Repository.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq.Expressions;
+using CompleteDotNetCore.DataAccess.Repository.IRepository;
+
+namespace CompleteDotNetCore.DataAccess.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext _db;
+
+        public Repository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public void Add(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetFirstOrDefault(Expression<Func<T, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveRange(IEnumerable<T> entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
