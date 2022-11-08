55791a7a36835f70508cd1548c717a98fb20cf39
Scott Isitt <isitts@hotmail.com>
2022-11-08 00:22:05 -0800

    Create ICategoryRepository with Update and Save methods.

---
+++ CompleteDotNetCore.DataAccess/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using CompleteDotNetCore.Models;
+
+namespace CompleteDotNetCore.DataAccess.Repository.IRepository
+{
+    public interface ICategoryRepository : IRepository<Category>
+    {
+        void Update(Category obj);
+        void Save();
+    }
+}
+
