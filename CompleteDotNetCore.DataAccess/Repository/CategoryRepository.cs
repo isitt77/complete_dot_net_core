aa783becb679477cf77c90e93e6d6a2cd430fad9
Scott Isitt <isitts@hotmail.com>
2022-11-08 01:17:28 -0800

    Create CategoryRepository class. Create Update and Save methods.

---
+++ CompleteDotNetCore.DataAccess/Repository/CategoryRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using CompleteDotNetCore.DataAccess.Repository.IRepository;
+using CompleteDotNetCore.Models;
+
+namespace CompleteDotNetCore.DataAccess.Repository
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+
+        public void Update(Category obj)
+        {
+            _db.Categories.Update(obj);
+        }
+    }
+}
+
