bea13c849381cbd882e4c9867d40cc4c2bf06e65
Scott Isitt <isitts@hotmail.com>
2022-11-13 23:30:33 -0800

    Add ICoverTypeRepository to UnitOfWork.

--- CompleteDotNetCore.DataAccess/Repository/IRepository/IUnitOfWork.cs
+++ CompleteDotNetCore.DataAccess/Repository/IRepository/IUnitOfWork.cs
@@ -4,6 +4,7 @@ namespace CompleteDotNetCore.DataAccess.Repository.IRepository
     public interface IUnitOfWork
     {
         ICategoryRepository Category { get; }
+        ICoverTypeRepository CoverType { get; }
         void Save();
     }
 }
