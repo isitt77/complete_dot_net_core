9576f1b610d145019faa7ffe6ac49f83b246343c
Scott Isitt <isitts@hotmail.com>
2022-11-13 23:01:05 -0800

    Create interface ICoverTypeRepository and class CoverTypeRepository.

---
+++ CompleteDotNetCore.DataAccess/Repository/CoverTypeRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using CompleteDotNetCore.DataAccess.Repository.IRepository;
+using CompleteDotNetCore.Models;
+
+namespace CompleteDotNetCore.DataAccess.Repository
+{
+    public class CoverTypeRepository : Repository<CoverType>, ICoverTypeRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CoverTypeRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(CoverType obj)
+        {
+            _db.CoverTypes.Update(obj);
+        }
+    }
+}
+
---
+++ CompleteDotNetCore.DataAccess/Repository/IRepository/ICoverTypeRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using CompleteDotNetCore.Models;
+
+namespace CompleteDotNetCore.DataAccess.Repository.IRepository
+{
+    public interface ICoverTypeRepository : IRepository<CoverType>
+    {
+        void Update(CoverType obj);
+    }
+}
+
