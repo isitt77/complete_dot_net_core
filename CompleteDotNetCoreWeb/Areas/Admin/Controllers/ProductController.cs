3d8eb687120d0b1f03debebef3b78f849ca0adba
Scott Isitt <isitts@hotmail.com>
2022-11-17 00:14:27 -0800

    Create Product Controller and View.

---
+++ CompleteDotNetCoreWeb/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CompleteDotNetCore.DataAccess.Repository.IRepository;
+using CompleteDotNetCore.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CompleteDotNetCoreWeb.Areas.Admin.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ProductController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+
+        public IActionResult Index()
+        {
+            IEnumerable<Product> objProductList =
+                _unitOfWork.Product.GetAll();
+            return View(objProductList);
+        }
+    }
+}
