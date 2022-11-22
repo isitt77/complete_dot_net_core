7be5bed5df07cd069f1f676949b1ffa186e647d5
Scott Isitt <isitts@hotmail.com>
2022-11-22 01:36:50 -0800

    Add Edit and Delete buttons to product.js. Currently not working.

--- CompleteDotNetCoreWeb/wwwroot/js/product.js
+++ CompleteDotNetCoreWeb/wwwroot/js/product.js
@@ -16,7 +16,25 @@ function loadDataTable() {
             { "data": "isbn", "width": "15%" },
             { "data": "price", "width": "15%" },
             { "data": "category.name", "width": "15%" },
-            { "data": "coverType.name", "width": "15%" }
+            { "data": "coverType.name", "width": "15%" },
+            {
+                "data": "id",
+                "render": function (data) {
+                    return
+                    `<td class="btn-group w-50%" role="button">
+                        <a class="btn btn-warning"
+                            href="/Admin/Product/Upsert?id=${data}">
+                        <i class="bi bi-pencil"></i> &nbsp; Edit</a>
+                     </td>
+                     <td class="btn-group w-50%" role="button">
+                         <a class="btn btn-danger"
+                             href="/Admin/Product/Delete?id=${data}">
+                          <i class="bi bi-trash"></i> &nbsp; Delete</a>
+                     </td>
+                     `
+                },
+                "width": "15%"
+            }
         ]
     });
 }
