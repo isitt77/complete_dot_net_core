c20a22145b20bff1f1903f70a9c73ea78ed6afb7
Scott Isitt <isitts@hotmail.com>
2022-11-22 23:12:07 -0800

    Adjust brackets in product.js to make more legible.

--- CompleteDotNetCoreWeb/wwwroot/js/product.js
+++ CompleteDotNetCoreWeb/wwwroot/js/product.js
@@ -10,18 +10,19 @@ function loadDataTable() {
         "ajax": {
             "url": "/Admin/Product/GetAll"
         },
-        "columns": [
-            { "data": "title", "width": "15%" },
-            { "data": "author", "width": "15%" },
-            { "data": "isbn", "width": "15%" },
-            { "data": "price", "width": "15%" },
-            { "data": "category.name", "width": "15%" },
-            { "data": "coverType.name", "width": "15%" },
-            {
-                "data": "id",
-                "render": function (data) {
-                    return
-                    `<td class="btn-group w-50%" role="button">
+        "columns":
+            [
+                { "data": "title", "width": "15%" },
+                { "data": "author", "width": "15%" },
+                { "data": "isbn", "width": "15%" },
+                { "data": "price", "width": "15%" },
+                { "data": "category.name", "width": "15%" },
+                { "data": "coverType.name", "width": "15%" },
+                {
+                    "data": "id",
+                    "render": function (data) {
+                        return
+                        `<td class="btn-group w-50%" role="button">
                         <a class="btn btn-warning"
                             href="/Admin/Product/Upsert?id=${data}">
                         <i class="bi bi-pencil"></i> &nbsp; Edit</a>
@@ -32,9 +33,9 @@ function loadDataTable() {
                           <i class="bi bi-trash"></i> &nbsp; Delete</a>
                      </td>
                      `
-                },
-                "width": "15%"
-            }
-        ]
+                    },
+                    "width": "15%"
+                }
+            ]
     });
 }
