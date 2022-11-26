ec8f86f05947daf49a1f13951d20da61bb91c447
Scott Isitt <isitts@hotmail.com>
2022-11-25 20:02:20 -0800

    Add your tags back into product.js function, but comment out.

--- CompleteDotNetCoreWeb/wwwroot/js/product.js
+++ CompleteDotNetCoreWeb/wwwroot/js/product.js
@@ -27,13 +27,22 @@ function loadDataTable() {
                     "render": function (data) {
                         console.log(data);
                         return `
-                        <div class="w-75 btn-group" role="group">
-                        <a href="/Admin/Product/Upsert?id=${data}"
-                        class="btn btn-warning mx-2"> <i class="bi bi-pencil"></i> Edit</a>
-                        <a 
-                        class="btn btn-danger mx-2"> <i class="bi bi-trash-fill"></i> Delete</a>
-					</div>
-                        `
+                            <div class="w-75 btn-group" role="group">
+                                <a href="/Admin/Product/Upsert?id=${data}"
+                                    class="btn btn-warning mx-2"> <i class="bi bi-pencil"></i> Edit</a>
+                                 <a class="btn btn-danger mx-2"> <i class="bi bi-trash-fill"></i> Delete</a>
+                        </div>`
+                        //   return `<td class="btn-group w-50%">
+                        //   <a class="btn btn-warning"
+                        //       href="/Admin/Product/Upsert?id=${data}">
+                        //   <i class="bi bi-pencil"></i> &nbsp; Edit</a>
+                        //</td>
+                        //<td class="btn-group w-50%" role="button">
+                        //    <a class="btn btn-danger"
+                        //        href="/Admin/Product/Delete?id=${data}">
+                        //     <i class="bi bi-trash"></i> &nbsp; Delete</a>
+                        //</td>
+                        //`
                     },
                     "width": "15%"
                 }
