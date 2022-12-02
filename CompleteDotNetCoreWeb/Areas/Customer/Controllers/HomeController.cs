a4647f25b5cbcef30898a668cc89ccdb7c1c051d
Scott Isitt <isitts@hotmail.com>
2022-12-02 00:14:03 -0800

    Add IUnitOfWork to Customer Home Controller constructor.

--- CompleteDotNetCoreWeb/Areas/Customer/Controllers/HomeController.cs
+++ CompleteDotNetCoreWeb/Areas/Customer/Controllers/HomeController.cs
@@ -1,16 +1,20 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using CompleteDotNetCore.Models;
+using CompleteDotNetCore.DataAccess.Repository.IRepository;
 
 namespace CompleteDotNetCoreWeb.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly IUnitOfWork _unitOfWork;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger,
+        IUnitOfWork unitOfWork)
     {
         _logger = logger;
+        _unitOfWork = unitOfWork;
     }
 
     public IActionResult Index()
