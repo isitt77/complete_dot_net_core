dc47a5ac74aed708272177672cbffe27c49caed1
Scott Isitt <isitts@hotmail.com>
2022-12-05 01:24:07 -0800

    Redo Identity Scaffold without specifying files.

--- CompleteDotNetCoreWeb/Program.cs
+++ CompleteDotNetCoreWeb/Program.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
 using Microsoft.Extensions.DependencyInjection;
+//using CompleteDotNetCoreWeb.Areas.Identity.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("ApplicationDbContextConnection") ?? throw new InvalidOperationException("Connection string 'ApplicationDbContextConnection' not found.");
