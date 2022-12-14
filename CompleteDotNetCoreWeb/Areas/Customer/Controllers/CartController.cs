da717c45a99f4f2a0562de7083a782eb90409748
Scott Isitt <isitts@hotmail.com>
2022-12-14 00:39:37 -0800

    Create CartController and Index View.

---
+++ CompleteDotNetCoreWeb/Areas/Customer/Controllers/CartController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace CompleteDotNetCoreWeb.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+    public class CartController : Controller
+    {
+        // GET: Cart Index
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
+
