08024fa06e46196552f209f011b4daa21fa13e29
Scott Isitt <isitts@hotmail.com>
2022-12-18 20:10:44 -0800

    Add properties to OrderHeader Model.

--- CompleteDotNetCore.Models/OrderHeader.cs
+++ CompleteDotNetCore.Models/OrderHeader.cs
@@ -1,9 +1,57 @@
 ﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+
 namespace CompleteDotNetCore.Models
 {
     public class OrderHeader
     {
+        public int Id { get; set; }
 
+        public string ApplicationUserId { get; set; }
+        [ForeignKey("ApplicationUserId")]
+        [ValidateNever]
+        public ApplicationUser ApplicationUser { get; set; }
+
+        [Required]
+        public DateTime OrderDate { get; set; }
+
+        public DateTime ShippingDate { get; set; }
+
+        public double OrderTotal { get; set; }
+
+        public string? OrderStatus { get; set; }
+
+        public string? PaymentStatus { get; set; }
+
+        public string? TrackingNumber { get; set; }
+
+        public string? Carrier { get; set; }
+
+        public DateTime PaymentDate { get; set; }
+
+        public DateTime PaymentDueDate { get; set; }
+
+        // Stripe Payment info...
+        public string? SessionId { get; set; }
+        public string? PaymentIntentId { get; set; }
+
+        // Shippping Address Info
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Address { get; set; }
+        [Required]
+        public string City { get; set; }
+        [Required]
+        public string State { get; set; }
+        [Required]
+        [DisplayName("Zip Code")]
+        public string ZipCode { get; set; }
+        [Required]
+        public string PhoneNumber { get; set; }
     }
 }
 
