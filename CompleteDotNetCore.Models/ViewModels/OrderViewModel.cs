bb4f6dd9e69b88f00190afbcda6805702922482d
Scott Isitt <isitts@hotmail.com>
2022-12-27 23:06:56 -0800

    Create OrderViewModel, OrderController, and Order Index View.

---
+++ CompleteDotNetCore.Models/ViewModels/OrderViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace CompleteDotNetCore.Models.ViewModels
+{
+    public class OrderViewModel
+    {
+        public OrderHeader OrderHeader { get; set; }
+        public IEnumerable<OrderDetail> OrderDetail { get; set; }
+    }
+}
+
---
+++ CompleteDotNetCoreWeb/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CompleteDotNetCore.DataAccess.Repository.IRepository;
+using CompleteDotNetCore.Models;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace CompleteDotNetCoreWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class OrderController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public OrderController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+
+        // GET: Order Index
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        #region API CALLS
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            IEnumerable<OrderHeader> orderHeaders;
+
+            orderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties:
+                "ApplicationUser");
+            return Json(new { data = orderHeaders });
+        }
+        #endregion
+    }
+}
+
