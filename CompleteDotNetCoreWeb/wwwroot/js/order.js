ee0ecc5cad8e76c7cfcb343912604a639b86f740
Scott Isitt <isitts@hotmail.com>
2022-12-28 00:19:23 -0800

    Create order.js.

---
+++ CompleteDotNetCoreWeb/wwwroot/js/order.js
@@ -0,0 +1,41 @@
+﻿var dataTable;
+
+$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    dataTable = $("#dataTable").DataTable({
+        columnDefs: [{
+            "defaultContent": "",
+            "targets": "_all"
+        }],
+
+        "ajax": {
+            "url": "/Admin/Order/GetAll"
+        },
+        "columns":
+            [
+                { "data": "id" },
+                { "data": "name" },
+                { "data": "phoneNumber" },
+                { "data": "applicationUser.email" },
+                { "data": "orderStatus" },
+                { "data": "orderTotal" },
+                {
+                    "data": "id",
+                    "render": function (data) {
+                        return `
+                        <td>
+                           <a class="btn btn-info mx-2"
+                               href="/Admin/Order/Details?orderId=${data}">
+                                    Details
+                           </a>
+                        </td>
+                        `
+                    }, "width": "24%"
+                }
+            ]
+    });
+}
+
