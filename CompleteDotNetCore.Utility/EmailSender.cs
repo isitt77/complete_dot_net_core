d9417cdd3844ac5b7325ce014c530d6fe0a00be3
Scott Isitt <isitts@hotmail.com>
2023-01-12 00:22:13 -0800

    Set up MimeKit and MailKit for email sender.

--- CompleteDotNetCore.Utility/EmailSender.cs
+++ CompleteDotNetCore.Utility/EmailSender.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using MailKit.Net.Smtp;
 using Microsoft.AspNetCore.Identity.UI.Services;
+using MimeKit;
 
 namespace CompleteDotNetCore.Utility
 {
@@ -12,6 +14,24 @@ namespace CompleteDotNetCore.Utility
         public Task SendEmailAsync(string email,
             string subject, string htmlMessage)
         {
+            MimeMessage emailToSend = new();
+            emailToSend.From.Add(MailboxAddress.Parse(
+                "helllo@dotnetcore.com"));
+            emailToSend.To.Add(MailboxAddress.Parse(email));
+            emailToSend.Subject = subject;
+            emailToSend.Body = new TextPart(MimeKit.Text.TextFormat
+                .Html)
+            { Text = htmlMessage };
+
+            // Send email
+            using (SmtpClient emailClient = new())
+            {
+                emailClient.Connect("smtp.gmail.com", 587, MailKit.Security
+                    .SecureSocketOptions.StartTls);
+                emailClient.Authenticate("dotnetcore@gmail.com", "Dotnet@123");
+                emailClient.Send(emailToSend);
+                emailClient.Disconnect(true);
+            }
             return Task.CompletedTask;
         }
     }
