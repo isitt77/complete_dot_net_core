30e5e66915b047b56716bc74173505f512337ab6
Scott Isitt <isitts@hotmail.com>
2023-01-14 23:01:12 -0800

    Create ShoppingCartViewComponent.

---
+++ CompleteDotNetCoreWeb/ViewComponents/ShoppingCartViewComponent.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Security.Claims;
+using CompleteDotNetCore.DataAccess.Repository.IRepository;
+using CompleteDotNetCore.Utility;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CompleteDotNetCoreWeb.ViewComponents
+{
+    public class ShoppingCartViewComponent : ViewComponent
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ShoppingCartViewComponent(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            ClaimsIdentity claimsIdentity = (ClaimsIdentity)User.Identity;
+            Claim claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+            // Checking if user is logged in...
+            if (claim != null)
+            {
+                // Checking Session...
+                if (HttpContext.Session.GetInt32(SD.SessionCart) != null)
+                {
+                    return View(HttpContext.Session.GetInt32(SD.SessionCart));
+                }
+                else
+                {
+                    // Assign new Session
+                    HttpContext.Session.SetInt32(SD.SessionCart, _unitOfWork
+                        .ShoppingCart.GetAll(u => u.ApplicationUserId
+                        == claim.Value).ToList().Count);
+
+                    return View(HttpContext.Session.GetInt32(SD.SessionCart));
+                }
+            }
+            else
+            {
+                HttpContext.Session.Clear();
+
+                return View();
+            }
+        }
+    }
+}
+
