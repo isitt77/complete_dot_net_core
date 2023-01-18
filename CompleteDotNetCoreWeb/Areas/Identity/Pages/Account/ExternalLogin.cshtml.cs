69428ce0a93c061d15baa9c120fc89481c8a30ae
Scott Isitt <isitts@hotmail.com>
2023-01-17 19:54:11 -0800

    Add more input properties to ExternalLogin Page/PageModel.

--- CompleteDotNetCoreWeb/Areas/Identity/Pages/Account/ExternalLogin.cshtml.cs
+++ CompleteDotNetCoreWeb/Areas/Identity/Pages/Account/ExternalLogin.cshtml.cs
@@ -17,6 +17,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.WebUtilities;
 using Microsoft.Extensions.Logging;
+using System.ComponentModel;
 
 namespace CompleteDotNetCoreWeb.Areas.Identity.Pages.Account
 {
@@ -84,8 +85,18 @@ namespace CompleteDotNetCoreWeb.Areas.Identity.Pages.Account
             [Required]
             [EmailAddress]
             public string Email { get; set; }
+
+            [Required]
+            public string Name { get; set; }
+            public string Address { get; set; }
+            public string City { get; set; }
+            public string State { get; set; }
+            [DisplayName("Zip Code")]
+            public string ZipCode { get; set; }
+            [DisplayName("Phone Number")]
+            public string PhoneNumber { get; set; }
         }
-        
+
         public IActionResult OnGet() => RedirectToPage("./Login");
 
         public IActionResult OnPost(string provider, string returnUrl = null)
