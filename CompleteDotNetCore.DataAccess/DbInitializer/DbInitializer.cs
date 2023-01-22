b4663490a054ea874fe6198d39943736ee68baf0
Scott Isitt <isitts@hotmail.com>
2023-01-22 00:21:30 -0800

    Addd dependency injection to DbInitializer class.

--- CompleteDotNetCore.DataAccess/DbInitializer/DbInitializer.cs
+++ CompleteDotNetCore.DataAccess/DbInitializer/DbInitializer.cs
@@ -1,10 +1,25 @@
 ﻿using System;
+using CompleteDotNetCore.DataAccess.Repository.IRepository;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.Extensions.Logging;
+
 namespace CompleteDotNetCore.DataAccess.DbInitializer
 {
     public class DbInitializer : IDbInitializer
     {
-        public DbInitializer()
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly ApplicationDbContext _db;
+
+        public DbInitializer(
+            UserManager<IdentityUser> userManager,
+            RoleManager<IdentityRole> roleManager,
+            ApplicationDbContext db)
         {
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _db = db;
         }
 
         public void Initialize()
