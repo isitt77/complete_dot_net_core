c5823b56cacb7878e6314774cb1c550dae57359c
Scott Isitt <isitts@hotmail.com>
2023-01-22 02:06:00 -0800

    Add InitializeAsync() logic in DbInitializer.

--- CompleteDotNetCore.DataAccess/DbInitializer/DbInitializer.cs
+++ CompleteDotNetCore.DataAccess/DbInitializer/DbInitializer.cs
@@ -1,8 +1,16 @@
 ﻿using System;
+using System.ComponentModel;
+using System.IO;
 using CompleteDotNetCore.DataAccess.Repository.IRepository;
+using CompleteDotNetCore.Models;
+using CompleteDotNetCore.Utility;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using static System.Formats.Asn1.AsnWriter;
+using static System.Net.Mime.MediaTypeNames;
+
 
 namespace CompleteDotNetCore.DataAccess.DbInitializer
 {
@@ -22,13 +30,45 @@ namespace CompleteDotNetCore.DataAccess.DbInitializer
             _db = db;
         }
 
-        public void Initialize()
+        public async Task InitializeAsync()
         {
             // Apply migrations, if not applied...
+            try
+            {
+                // If there are migrations that have not been applied...
+                if (_db.Database.GetPendingMigrations().Count() > 0)
+                {
+                    _db.Database.Migrate();
+                }
+            }
+            catch (Exception exception)
+            {
+
+            }
 
             // Create Roles, if not created...
+            if (!_roleManager.RoleExistsAsync(SD.RoleAdmin).GetAwaiter().GetResult())
+            {
+                await _roleManager.CreateAsync(new IdentityRole(SD.RoleAdmin));
+                await _roleManager.CreateAsync(new IdentityRole(SD.RoleEmployee));
+                await _roleManager.CreateAsync(new IdentityRole(SD.RoleUserComp));
+                await _roleManager.CreateAsync(new IdentityRole(SD.RoleUserIndv));
 
-            // If Roles not created, create Admin user as well.
+                // If Roles not created, create Admin user as well.
+                await _userManager.CreateAsync(new ApplicationUser
+                {
+                    UserName = "isitts@hotmail.com",
+                    Email = "isitts@hotmail.com",
+                    Name = "Scott Isitt",
+                    PhoneNumber = "1112223333",
+                    Address = "123 Test Street",
+                    City = "Citytown",
+                    State = "New Staton",
+                    ZipCode = "99999"
+                }, "Admin@123");
+            }
+
+
 
             throw new NotImplementedException();
         }
