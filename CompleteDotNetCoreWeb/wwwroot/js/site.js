40181b11c032ac1f32afe6313550332588b06ebc
Scott Isitt <isitts@hotmail.com>
2023-02-01 18:26:07 -0800

    Change switch to individual buttons to change light and dark mode. Initial setup.

--- CompleteDotNetCoreWeb/wwwroot/js/site.js
+++ CompleteDotNetCoreWeb/wwwroot/js/site.js
@@ -3,40 +3,51 @@
 
 // Write your JavaScript code.
 
-function LightDarkToggle() {
-    // get element by id
-
-    let settingSwitch = document.getElementById("flexSwitchCheckDefault");
-    console.log(settingSwitch.id);
-
-    let label = settingSwitch.nextElementSibling;
-    console.log(label.innerHTML);
-
-    let isChecked = settingSwitch.hasAttribute("checked");
-    console.log(isChecked);
-
-    let styleLink = document.getElementById("dark-light-bootst-style").attributes[2];
-    console.log(styleLink.value);
-    console.log(`Key ${styleLink.name}`);
 
 
-
-    if (isChecked) {
-        console.log("Light mode active.");
-        settingSwitch.removeAttribute("checked");
-        label.innerHTML = "Light";
-        styleLink.value = "/lib/bootstrap/dist/css/light/bootstrap.min.css";
-        localStorage.setItem(styleLink.name, styleLink.value);
-    }
-    else {
-        console.log("Dark mode active.");
-        settingSwitch.setAttribute("checked", "");
-        label.innerHTML = "Dark";
-        styleLink.value = "/lib/bootstrap/dist/css/dark/bootstrap.min.css";
-        localStorage.setItem(styleLink.name, styleLink.value);
-    }
+function MakeDark() {
 
 }
+
+function MakeLight() {
+
+}
+
+
+//function LightDarkToggle() {
+//    // get element by id
+
+//    let settingSwitch = document.getElementById("flexSwitchCheckDefault");
+//    console.log(settingSwitch.id);
+
+//    let label = settingSwitch.nextElementSibling;
+//    console.log(label.innerHTML);
+
+//    let isChecked = settingSwitch.hasAttribute("checked");
+//    console.log(isChecked);
+
+//    let styleLink = document.getElementById("dark-light-bootst-style").attributes[2];
+//    console.log(styleLink.value);
+//    console.log(`Key ${styleLink.name}`);
+
+
+
+//    if (isChecked) {
+//        console.log("Light mode active.");
+//        settingSwitch.removeAttribute("checked");
+//        label.innerHTML = "Light";
+//        styleLink.value = "/lib/bootstrap/dist/css/light/bootstrap.min.css";
+//        localStorage.setItem(styleLink.name, styleLink.value);
+//    }
+//    else {
+//        console.log("Dark mode active.");
+//        settingSwitch.setAttribute("checked", "");
+//        label.innerHTML = "Dark";
+//        styleLink.value = "/lib/bootstrap/dist/css/dark/bootstrap.min.css";
+//        localStorage.setItem(styleLink.name, styleLink.value);
+//    }
+
+//}
 
 
 
