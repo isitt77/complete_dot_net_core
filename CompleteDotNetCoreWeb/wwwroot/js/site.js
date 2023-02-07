7ffb181ebb0d8aa8b2001487d096fba1e2052ebe
Scott Isitt <isitts@hotmail.com>
2023-02-06 22:58:08 -0800

    Delete unused variables from ligh/dark theme logic. Comment out remaining console logs.

--- CompleteDotNetCoreWeb/wwwroot/js/site.js
+++ CompleteDotNetCoreWeb/wwwroot/js/site.js
@@ -4,33 +4,22 @@
 // Write your JavaScript code.
 
 
-
-//var checkbox = document.querySelector("input[name=checkbox]");
-
-//checkbox.addEventListener('change', function () {
-//    if (this.checked) {
-//        console.log("Checkbox is checked..");
-//    } else {
-//        console.log("Checkbox is not checked..");
-//    }
-//});
-
 let styleLink = document.getElementById("dark-light-bootst-style");
 let styleLinkRefAttribute = document.getElementById("dark-light-bootst-style").attributes[2];
-let styleLinkRefAttributes = document.getElementById("dark-light-bootst-style").attributes;
 
 let darkStyleUrl = "/lib/bootstrap/dist/css/dark/bootstrap.min.css";
 let lightStyleUrl = "/lib/bootstrap/dist/css/light/bootstrap.min.css";
 
 let getStyle = localStorage.getItem(styleLinkRefAttribute.name);
-console.log(`getStyle: ${getStyle}`);
+//console.log(`getStyle: ${getStyle}`);
 
 let isDark = getStyle == darkStyleUrl;
-console.log(`isDark: ${isDark}`);
+//console.log(`isDark: ${isDark}`);
 
 let lightDarkCheckbox = document.querySelector("input[name=lightDarkCheck]");
 
-let isChecked = lightDarkCheckbox.checked;
+let label = lightDarkCheckbox.nextElementSibling;
+//console.log(`label: ${label.innerHTML}`);
 
 
 SetTheme();
@@ -41,24 +30,28 @@ lightDarkCheckbox.addEventListener('change', LightDarkToggle);
 function SetTheme() {
     if (isDark) {
         lightDarkCheckbox.setAttribute("checked", "");
-        console.log("Checkbox is checked..");
+        //console.log("Checkbox is checked..");
 
         styleLinkRefAttribute.value = darkStyleUrl;
         let darkStyleValue = styleLinkRefAttribute.value;
 
         localStorage.setItem(styleLinkRefAttribute.name, darkStyleValue);
 
-        console.log(`isDark: ${isDark}`);
+        label.innerHTML = "Dark";
+
+        //console.log(`isDark: ${isDark}`);
     } else {
         lightDarkCheckbox.removeAttribute("checked");
-        console.log("Checkbox is not checked..");
+        //console.log("Checkbox is not checked..");
 
         styleLinkRefAttribute.value = lightStyleUrl;
         let lightStyleValue = styleLinkRefAttribute.value;
 
         localStorage.setItem(styleLinkRefAttribute.name, lightStyleValue);
 
-        console.log(`isDark: ${isDark}`);
+        label.innerHTML = "Light";
+
+        //console.log(`isDark: ${isDark}`);
     }
 }
 
@@ -68,3 +61,14 @@ function LightDarkToggle() {
     SetTheme();
 }
 
+
+
+//var checkbox = document.querySelector("input[name=checkbox]");
+
+//checkbox.addEventListener('change', function () {
+//    if (this.checked) {
+//        console.log("Checkbox is checked..");
+//    } else {
+//        console.log("Checkbox is not checked..");
+//    }
+//});
